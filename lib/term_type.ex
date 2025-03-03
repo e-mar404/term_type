4ab569055518ebeec71cca25919bf2c6fdac9a95
e-mar404 <egmarinn@gmail.com>
2025-03-03 12:19:14 -0600

    init term_type

---
+++ lib/term_type.ex
@@ -0,0 +1,18 @@
+defmodule TermType do
+  @moduledoc """
+  Documentation for `TermType`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TermType.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/term_type_test.exs
@@ -0,0 +1,8 @@
+defmodule TermTypeTest do
+  use ExUnit.Case
+  doctest TermType
+
+  test "greets the world" do
+    assert TermType.hello() == :world
+  end
+end
