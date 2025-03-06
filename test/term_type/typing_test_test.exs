f893c360997dc578a7d00fe9b473785fb242cb3b
e-mar404 <egmarinn@gmail.com>
2025-03-06 14:05:20 -0600

    start over
    adding what a document in ./test to outline how things need to work

--- test/term_type/typing_test_test.exs
+++ test/term_type/typing_test_test.exs
@@ -1,15 +1,6 @@
-defmodule Todo.ListTest do
+defmodule Todo.TypingTestTest do
   use ExUnit.Case, async: true
 
-  test "see if i get the updated state" do
-    TermType.TextBank.start_link()
-
-    TermType.TypingTest.start_link()
-    |> TermType.TypingTest.attempt("t")
-    |> TermType.TypingTest.attempt("h")
-    |> TermType.TypingTest.attempt("e")
-    |> TermType.TypingTest.attempt("\b")
-    |> TermType.TypingTest.attempt("\b")
-    |> TermType.TypingTest.render()
+  test "" do
   end
 end
