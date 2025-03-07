77ac3dc742dd41b2e5f4afa0c2ad65b773879ff3
e-mar404 <egmarinn@gmail.com>
2025-03-06 22:22:16 -0600

    implement minimal test for correct input
    yes this is tdd ... sadly

--- lib/term_type/typing_test.ex
+++ lib/term_type/typing_test.ex
@@ -1,2 +1,37 @@
 defmodule TermType.TypingTest do
+  defstruct cur_index: 0, letter_map: %{}, status_map: %{}
+
+  def new(initial_text) do
+    letter_list = 
+      initial_text
+      |> String.split("", trim: true)
+      |> Enum.with_index()
+
+    letter_map =
+      for {letter, index} <- letter_list, into: %{}, do: {index, letter}
+
+    status_map =
+      for {_letter, index} <- letter_list, into: %{}, do: {index, :background}
+
+    %TermType.TypingTest{letter_map: letter_map, status_map: status_map}
+  end
+
+  def attempt(test, attempt) do
+    {status, next_index} = check(test.letter_map, test.cur_index, attempt)
+
+    new_status_map = Map.put(test.status_map, test.cur_index, status)
+
+    %TermType.TypingTest{ test |
+      cur_index: next_index,
+      status_map: new_status_map
+    }
+  end
+
+  defp check(letter_map, index, attempt) do
+    correct = Map.get(letter_map, index)  
+
+    cond do
+      correct == attempt -> {:correct, index + 1}
+    end 
+  end
 end
--- test/term_type/typing_test_test.exs
+++ test/term_type/typing_test_test.exs
@@ -1,6 +1,52 @@
-defmodule Todo.TypingTestTest do
+defmodule TermType.TypingTestTest do
   use ExUnit.Case, async: true
 
-  test "" do
+  setup do
+    typing_test = TermType.TypingTest.new("type test")
+
+    letter_map = %{
+      0 => "t",
+        1 => "y", 
+        2 => "p",
+        3 => "e",
+        4 => " ",
+        5 => "t",
+        6 => "e",
+        7 => "s",
+        8 => "t"
+    }
+
+    status_map = %{
+      0 => :background,
+        1 => :background, 
+        2 => :background,
+        3 => :background,
+        4 => :background,
+        5 => :background,
+        6 => :background,
+        7 => :background,
+        8 => :background
+    }
+
+    { 
+      :ok,
+      %{
+        typing_test: typing_test,
+        letter_map: letter_map,
+        status_map: status_map
+      }
+    }
+  end
+
+  test "create a test with given input", context do
+    assert context.typing_test.letter_map == context.letter_map
+    assert context.typing_test.status_map == context.status_map
+  end
+
+  test "correct input as the first try", context do
+    new_test = TermType.TypingTest.attempt(context.typing_test, "t")
+    updated_status_map = Map.put(context.status_map, 0, :correct)
+
+    assert new_test.status_map == updated_status_map
   end
 end
